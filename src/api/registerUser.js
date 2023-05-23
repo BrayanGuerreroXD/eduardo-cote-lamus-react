fe562494a1e7778d1032aa095b2503036a30eadd
BrayanGuerreroXD <bg7047653@gmail.com>
2023-05-23 10:56:52 -0500

    Remove console.log

--- src/api/registerUser.js
+++ src/api/registerUser.js
@@ -31,9 +31,6 @@ const registerUSer = async (user_id) => {
       }
       return response.json();
     })
-    .then((data) => {
-      console.log(data);
-    })
     .catch((error) => {
       console.error("There was a problem with the fetch operation:", error);
     });
